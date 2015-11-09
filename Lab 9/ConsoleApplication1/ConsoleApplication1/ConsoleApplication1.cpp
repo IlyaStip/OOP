833ecdc9f6800cf0860e5c6d1af077870aee5035
Anton <samantond@mail.ru>
2015-11-09 21:49:43 +0300

    Lab 9

--- Lab 9/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Lab 9/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <vector>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	std::vector<double> vectors;
+	vectors.push_back(1);
+	vectors.push_back(2);
+	vectors.push_back(3);
+
+	vectors.pop_back();
+
+	std::cout << vectors[1] << std::endl;
+	vectors[1] = 7;
+	std::cout << vectors[1] << std::endl << std::endl;
+
+	std::vector<double>::iterator p = vectors.begin();
+	while (p != vectors.end()) 
+	{
+		std::cout << *p << std::endl; 
+		p++;
+	}
+
+	system("pause");
 	return 0;
 }
 
